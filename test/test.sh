2b721f9ace813e00358f5b8768b64f566729560a
philipz <philipzheng@gmail.com>
2017-05-11 01:29:20 +0800

    add bash & fix.

--- test/test.sh
+++ test/test.sh
@@ -1,17 +1,18 @@
-#!/bin/sh                                                                                                                                                                                                     
+#!/bin/bash
 IFS=" "
 export IFS;
 words=$(cat result.txt | grep Request)
 rsnum=${words##* }
 echo $rsnum
-std=400.0
+std=50.0
 if (( $(echo "$rsnum > $std" | bc -l) )); then
 	echo -e "[-----------------------"
 	echo -e "[Performace Tests passed"
 	echo -e "[-----------------------"
         exit 0
-fi
+else
 	echo -e "[-----------------------"
 	echo -e "[Performace Tests failed"
 	echo -e "[-----------------------"
         exit 1
+fi
