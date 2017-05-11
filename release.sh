d910aa0ba00220fad8c27ac757ae5ca8e0b2394c
鄭淳尹 <cycheng3@vghtpe.gov.tw>
2017-05-11 08:40:41 +0800

    Change back to 'az webapp config container update'.

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 az login --service-principal -u $AZURE_LOGIN_USER --password $AZURE_PASSWORD --tenant $AZURE_TENANT
-az appservice web config container set -n tradingbot -g tradingbot -c dockware.azurecr.io/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG -r https://dockware.azurecr.io -u dockware -p $AZURE_REG_PASSWORD
+az appservice web config container update -n tradingbot -g tradingbot -c dockware.azurecr.io/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG -r https://dockware.azurecr.io -u dockware -p $AZURE_REG_PASSWORD
